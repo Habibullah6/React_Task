692de89b30bfa901aa6460a7d4d0fa90f7a925cf
DESKTOP-TB6HHOS\SARK <nomanhabibullah65@gmail.com>
2023-10-06 22:02:47 +0600

    done

--- src/components/Problem-1.jsx
+++ src/components/Problem-1.jsx
@@ -1,59 +1,125 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 
 const Problem1 = () => {
+  const [name, setName] = useState("");
+  const [status, setStatus] = useState("");
+  const [tasks, setTasks] = useState([]);
+  const [show, setShow] = useState("all");
 
-    const [show, setShow] = useState('all');
+  const handleClick = (name, status) => {
+    const newTask = { name, status };
+    setTasks([...tasks, newTask]);
+    setName("");
+    setStatus("");
+  };
 
-    const handleClick = (val) =>{
-        setShow(val);
+  const filterTasks = () => {
+    if (show === "all") {
+      return tasks.sort((a, b) => {
+        if (a.status === "active" && b.status !== "active") return -1;
+        if (
+          a.status === "completed" &&
+          b.status !== "active" &&
+          b.status !== "completed"
+        )
+          return -1;
+        return 1;
+      });
+    } else if (show === "active") {
+      return tasks.filter((task) => task.status === "active");
+    } else if (show === "completed") {
+      return tasks.filter((task) => task.status === "completed");
     }
+    return tasks;
+  };
 
-    return (
-
-        <div className="container">
-            <div className="row justify-content-center mt-5">
-                <h4 className='text-center text-uppercase mb-5'>Problem-1</h4>
-                <div className="col-6 ">
-                    <form className="row gy-2 gx-3 align-items-center mb-4">
-                        <div className="col-auto">
-                            <input type="text" className="form-control" placeholder="Name"/>
-                        </div>
-                        <div className="col-auto">
-                            <input type="text" className="form-control" placeholder="Status"/>
-                        </div>
-                        <div className="col-auto">
-                            <button type="submit" className="btn btn-primary">Submit</button>
-                        </div>
-                    </form>
-                </div>
-                <div className="col-8">
-                    <ul className="nav nav-pills mb-3" id="pills-tab" role="tablist">
-                        <li className="nav-item">
-                            <button  className={`nav-link ${show==='all' && 'active'}`} type="button" onClick={()=>handleClick('all')}>All</button>
-                        </li>
-                        <li className="nav-item">
-                            <button className={`nav-link ${show==='active' && 'active'}`} type="button" onClick={()=>handleClick('active')}>Active</button>
-                        </li>
-                        <li className="nav-item">
-                            <button  className={`nav-link ${show==='completed' && 'active'}`} type="button" onClick={()=>handleClick('completed')}>Completed</button>
-                        </li>
-                    </ul>
-                    <div className="tab-content"></div>
-                    <table className="table table-striped ">
-                        <thead>
-                        <tr>
-                            <th scope="col">Name</th>
-                            <th scope="col">Status</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        
-                        </tbody>
-                    </table>
-                </div>
+  return (
+    <div className="container">
+      <div className="row justify-content-center mt-5">
+        <h4 className="text-center text-uppercase mb-5">Problem-1</h4>
+        <div className="col-6 ">
+          <form
+            className="row gy-2 gx-3 align-items-center mb-4"
+            onSubmit={(e) => {
+              e.preventDefault();
+              handleClick(name, status);
+            }}
+          >
+            <div className="col-auto">
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
             </div>
+            <div className="col-auto">
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Status"
+                value={status}
+                onChange={(e) => setStatus(e.target.value)}
+              />
+            </div>
+            <div className="col-auto">
+              <button type="submit" className="btn btn-primary">
+                Submit
+              </button>
+            </div>
+          </form>
         </div>
-    );
+        <div className="col-8">
+          <ul className="nav nav-pills mb-3" id="pills-tab" role="tablist">
+            <li className="nav-item">
+              <button
+                className={`nav-link ${show === "all" && "active"}`}
+                type="button"
+                onClick={() => setShow("all")}
+              >
+                All
+              </button>
+            </li>
+            <li className="nav-item">
+              <button
+                className={`nav-link ${show === "active" && "active"}`}
+                type="button"
+                onClick={() => setShow("active")}
+              >
+                Active
+              </button>
+            </li>
+            <li className="nav-item">
+              <button
+                className={`nav-link ${show === "completed" && "active"}`}
+                type="button"
+                onClick={() => setShow("completed")}
+              >
+                Completed
+              </button>
+            </li>
+          </ul>
+          <table className="table table-striped">
+            <thead>
+              <tr>
+                <th scope="col">Name</th>
+                <th scope="col">Status</th>
+              </tr>
+            </thead>
+            <tbody>
+              {filterTasks().map((task, index) => (
+                <tr key={index}>
+                  <td>{task.name}</td>
+                  <td>{task.status}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Problem1;
--- src/components/Problem-2.jsx
+++ src/components/Problem-2.jsx
@@ -1,21 +1,193 @@
-import React from 'react';
+// import React, { useState } from "react";
+// import Modal from "react-modal";
+// import axios from "axios";
+
+// const Problem2 = () => {
+//   const [modalAIsOpen, setModalAIsOpen] = useState(false);
+//   const [modalBIsOpen, setModalBIsOpen] = useState(false);
+//   const [contacts, setContacts] = useState([]);
+//   const [usContacts, setUsContacts] = useState([]);
+
+//   console.log(contacts);
+//   const openModalA = () => {
+//     axios
+//       .get("https://contact.mediusware.com/api/contacts/")
+//       .then((response) => {
+//         setContacts(response.data.results);
+//         setModalAIsOpen(true);
+//       })
+//       .catch((error) => console.error(error));
+//   };
+
+//   const openModalB = () => {
+//     axios
+//       .get(
+//         "https://contact.mediusware.com/api/country-contacts/united%20states/"
+//       )
+//       .then((response) => {
+//         setUsContacts(response.data.results);
+//         setModalBIsOpen(true);
+//       })
+//       .catch((error) => console.error(error));
+//   };
+
+//   return (
+//     <div className="container">
+//       <div className="row justify-content-center mt-5">
+//         <h4 className="text-center text-uppercase mb-5">Problem-2</h4>
+
+//         <div className="d-flex justify-content-center gap-3">
+//           <button
+//             className="btn btn-lg btn-outline-primary"
+//             type="button"
+//             onClick={openModalA}
+//           >
+//             All Contacts
+//           </button>
+//           <button
+//             className="btn btn-lg btn-outline-warning"
+//             type="button"
+//             onClick={openModalB}
+//           >
+//             US Contacts
+//           </button>
+//         </div>
+
+//         <Modal
+//           isOpen={modalAIsOpen}
+//           onRequestClose={() => setModalAIsOpen(false)}
+//         >
+//           <h2>Modal A</h2>
+
+//           <ul>
+//             {contacts.map((contact) => (
+//               <li key={contact.id}>{contact.phone}</li>
+//             ))}
+//           </ul>
+//         </Modal>
+
+//         <Modal
+//           isOpen={modalBIsOpen}
+//           onRequestClose={() => setModalBIsOpen(false)}
+//         >
+//           <h2>Modal B</h2>
+//           <ul>
+//             {usContacts.map((contact) => (
+//               <li key={contact.id}>{contact.phone}</li>
+//             ))}
+//           </ul>
+//         </Modal>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Problem2;
+
+import React, { useState } from "react";
+import Modal from "react-modal";
+import axios from "axios";
 
 const Problem2 = () => {
+  const [modalAIsOpen, setModalAIsOpen] = useState(false);
+  const [modalBIsOpen, setModalBIsOpen] = useState(false);
+  const [contacts, setContacts] = useState([]);
+  const [usContacts, setUsContacts] = useState([]);
 
-    return (
+  const openModalA = () => {
+    axios
+      .get("https://contact.mediusware.com/api/contacts/")
+      .then((response) => {
+        setContacts(response.data.results);
+        setModalAIsOpen(true);
+      })
+      .catch((error) => console.error(error));
+  };
 
-        <div className="container">
-            <div className="row justify-content-center mt-5">
-                <h4 className='text-center text-uppercase mb-5'>Problem-2</h4>
-                
-                <div className="d-flex justify-content-center gap-3">
-                <button className="btn btn-lg btn-outline-primary" type="button" >All Contacts</button>
-                <button className="btn btn-lg btn-outline-warning" type="button" >US Contacts</button>
-                </div>
-                
-            </div>
+  const openModalB = () => {
+    axios
+      .get(
+        "https://contact.mediusware.com/api/country-contacts/united%20states/"
+      )
+      .then((response) => {
+        setUsContacts(response.data.results);
+        setModalBIsOpen(true);
+      })
+      .catch((error) => console.error(error));
+  };
+
+  return (
+    <div className="container">
+      <div className="row justify-content-center mt-5">
+        <h4 className="text-center text-uppercase mb-5">Problem-2</h4>
+
+        <div className="d-flex justify-content-center gap-3">
+          <button
+            className="btn btn-lg btn-outline-primary"
+            type="button"
+            onClick={openModalA}
+            style={{ borderColor: "#46139f", color: "#46139f" }}
+          >
+            All Contacts
+          </button>
+          <button
+            className="btn btn-lg btn-outline-warning"
+            type="button"
+            onClick={openModalB}
+            style={{ borderColor: "#ff7f50", color: "#ff7f50" }}
+          >
+            US Contacts
+          </button>
         </div>
-    );
+
+        <Modal
+          isOpen={modalAIsOpen}
+          onRequestClose={() => setModalAIsOpen(false)}
+        >
+          <h2>Modal A</h2>
+          <ul>
+            {contacts.map((contact) => (
+              <li key={contact.id}>{contact.phone}</li>
+            ))}
+          </ul>
+          <button
+            className="btn"
+            onClick={() => setModalAIsOpen(false)}
+            style={{
+              borderColor: "#46139f",
+              color: "#46139f",
+              backgroundColor: "white",
+            }}
+          >
+            Close
+          </button>
+        </Modal>
+
+        <Modal
+          isOpen={modalBIsOpen}
+          onRequestClose={() => setModalBIsOpen(false)}
+        >
+          <h2>Modal B</h2>
+          <ul>
+            {usContacts.map((contact) => (
+              <li key={contact.id}>{contact.phone}</li>
+            ))}
+          </ul>
+          <button
+            className="btn"
+            onClick={() => setModalBIsOpen(false)}
+            style={{
+              borderColor: "#46139f",
+              color: "#46139f",
+              backgroundColor: "white",
+            }}
+          >
+            Close
+          </button>
+        </Modal>
+      </div>
+    </div>
+  );
 };
 
 export default Problem2;
